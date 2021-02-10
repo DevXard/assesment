8135ec7c74cea65597d63316bbe810c0dd88810b
Pavlin <forex.xard@gmail.com>
2021-02-10 17:00:40 -0500

    adding documentation

--- broken-app/app.js
+++ broken-app/app.js
@@ -8,25 +8,28 @@ app.use(express.json());
 app.post('/', async function(req, res, next) {
   
   try {
+    // send paralel requests and resolve them
     let results = await Promise.all(req.body.developers.map(d => 
     {
       return axios.get(`https://api.github.com/users/${d}`);
     }
     ))
-    
+    // map over the data from result and get name and bio
     let out = results.map(  r => ({ name:  r.data.name, bio: r.data.bio })
     );
-    
+    // send json response and stop the function
     return res.json(out);
   } catch(err) {
     next(err);
   }
 });
 
+//return 404  if rout is missing
 app.use((req, res, next) => {
   throw new ExpressError("not Found", 404)
 })
 
+// if there is and error from chatch return status code and message
 app.use((err, req, res, next) => {
   res.status(err.status || 500)
 
